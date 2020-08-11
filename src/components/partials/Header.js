31cfef4c31044b31e1751524e4b2b8d9e8f946e2
yogeshk <yogesh@binarynumbers.io>
2020-08-11 18:27:54 +0530

    Nav link issue fixed

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { Nav } from 'react-bootstrap';
 
 function Header() {
     return (
         <div>
-            <Nav variant="pills" className="justify-content-center">
+            <Nav variant="pills" className="justify-content-center" defaultActiveKey="link-1">
                 <Nav.Item>
-                    <Nav.Link href="/">Without Hooks</Nav.Link>
+                    <Nav.Link eventKey="link-1" as={Link} to="/">Without Hooks</Nav.Link>
                 </Nav.Item>
                 <Nav.Item >
-                    <Nav.Link href="/with-hooks">With Hooks</Nav.Link>
+                    <Nav.Link eventKey="link-2" as={Link} to="/with-hooks">With Hooks</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/combined-reducer">Combined Reducer</Nav.Link>
+                    <Nav.Link eventKey="link-3" as={Link} to="/combined-reducer">Combined Reducer</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/async-function">Async Function</Nav.Link>
+                    <Nav.Link eventKey="link-4" as={Link} to="/async-function">Async Function</Nav.Link>
                 </Nav.Item>
             </Nav>
         </div>
